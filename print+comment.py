1f0e4e086b04846c63885afbe0eb4ec0a41f8379
JANNATUL FERDOUS <60469756+RituJannat@users.noreply.github.com>
2025-09-28 17:01:39 +0600

    Add files via upload

---
+++ print+comment.py
@@ -0,0 +1,11 @@
+print("Hello Jannat,Welcome to pyhton Learning Step")
+#This is comment
+'this is comment - 2nd type of comment'
+print("how are you") #comment with print end single commnet
+print("what are you doing")
+print("How are you")
+"""
+multiple comment can apply in 3 quations
+& its helpful for more text 
+"""
+
---
+++ variable.py
@@ -0,0 +1,31 @@
+num = 10
+x , y,z= "jannat", "ritu", "ruhani"
+name = num
+m= x+y+z
+print(name)
+
+print(m)
+print(x)
+print(x+y)
+print(x+z)
+#now we see how many types of variable
+l= 4 #it is int type
+n= "onni" # it is string type
+print(type(l))
+print(type(n))
+z= float (3.9)
+print(type(z))
+a= "mehedi"
+A= "xannat"
+print(a + A)
+#we know python is case sensitive ,so a doesnot effect A
+o=P=q = "apple","banana","orange"
+fruite_name = o,P,q
+print(fruite_name)
+#unfack list
+fruits="fol","mango","goava"
+
+
+
+
+
