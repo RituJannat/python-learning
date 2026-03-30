e4615e0edf84ed7124220a62ba6702f71ef098b9
JANNATUL FERDOUS <60469756+RituJannat@users.noreply.github.com>
2026-03-30 17:42:19 +0600

    Add files via upload

---
+++ check.py
@@ -0,0 +1,13 @@
+marks = tem_mark = "100"
+marks = "0"
+va = 30.55
+a = int (tem_mark)
+b = int (marks)
+print (float(va))
+
+r,s,y= 10, 15, 20
+m= s+y
+print(m)
+f=z=t=5
+print (m+t)
+print (y+z)
---
+++ conditonalStatement.py
@@ -0,0 +1,32 @@
+''' number=5
+if number >0:
+    print("Positive")
+
+if number <7:
+    print("negative") '''
+mark=90
+if mark > 79 :
+ print("A+")
+if mark >69:
+    print("A-")
+if mark >69:
+    print("A")   
+
+
+if mark := 90:
+
+ print("Golden GPA 5")
+else:
+
+ print ("Sorry Try Next Time")
+#--------
+
+nums=  int(input("Enter Your Number:"))
+
+if nums % 2 ==0:
+  print("Even")
+else:
+  print("Odd")  
+
+  #--------------------
+  
